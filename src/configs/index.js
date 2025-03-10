e6119ebda61bbcee41a76a08412495e77c46d7b4
hoanggggf <vuhoang250203@gmail.com>
2025-03-10 08:21:07 +0700

    u

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,10 +1,10 @@
 const configs = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
   MONGOOSE: {
-    PORT: process.env.DB_PORT,
-    HOST: process.env.DB_HOST,
-    NAME: process.env.DB_NAME,
+    PORT: process.env.DB_PORT || 27017, // Thường MongoDB chạy trên cổng 27017
+    HOST: process.env.DB_HOST || "localhost",
+    NAME: process.env.DB_NAME || "my_database",
   },
 };
 
-module.exports = {configs}
+module.exports = { configs };
